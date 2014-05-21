58211f2397163d852433d0a182cb9bb6919e6005
Carlos Fuentes <cfuentes@dalai.com>
2014-05-21 00:25:48 -0500

    se agregaron relaciones

--- app/models/p_config/product.rb
+++ app/models/p_config/product.rb
@@ -1,5 +1,6 @@
 module PConfig
   class Product < ActiveRecord::Base
+    belongs_to :product_type
     default_value_for :is_enabled, true
   end
 end
--- app/models/p_config/standard.rb
+++ app/models/p_config/standard.rb
@@ -1,5 +1,6 @@
 module PConfig
   class Standard < ActiveRecord::Base
+    belongs_to :boot_variable
   end
 end
 
